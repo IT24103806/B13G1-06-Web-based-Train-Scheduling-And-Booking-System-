228173094666290a2b559e1381ac16d6e8444b4f
IT24103684 <IT24103684@my.sliit.lk>
2025-09-27 17:11:40 +0530

    Updated the User.java file

--- src/main/java/com/tsbs/model/User.java
+++ src/main/java/com/tsbs/model/User.java
@@ -1,15 +1,15 @@
 package com.tsbs.model;
 
-public class User {
-    private int userId;
-    private String email;
-    private String passwordHash;
-    private String name;
-    private String role;
+public class User  {
+    private int  userId;
+    private String  email;
+    private String  passwordHash;
+    private String  name;
+    private String  role;
 
-    public User() {}
+    public User () {}
 
-    public User(int userId, String email, String passwordHash, String name, String role) {
+    public  User(int userId, String email, String passwordHash, String name, String role) {
         this.userId = userId;
         this.email = email;
         this.passwordHash = passwordHash;
@@ -17,18 +17,34 @@ public class User {
         this.role = role;
     }
 
-    public int getUserId() { return userId; }
-    public void setUserId(int userId) { this.userId = userId; }
+    public int getUserId () {
+        return userId; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setUserId(int userId) {
+        this.userId = userId; }
 
-    public String getPasswordHash() { return passwordHash; }
-    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+    public String getEmail () {
+        return email; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) {
+        this.email = email; }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getPasswordHash () {
+        return passwordHash; }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash; }
+
+    public String getName () {
+        return name; }
+
+    public void setName(String name) {
+        this.name = name; }
+
+    public String getRole () {
+        return role; }
+
+    public void setRole(String role) {
+        this.role = role; }
+
 }
